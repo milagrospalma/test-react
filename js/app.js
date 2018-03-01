88be6bcd44290131e0e2d2e85176e719321bd1df
milagrospalma <mnpalma.2@gmail.com>
2018-03-01 05:47:32 -0500

    añade prueba 1

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
-const el = React.createElement('h3', {id:'title'}, React.createElement('span', null, 'hey'))
+// const el = React.createElement(
+//   'h3',
+//   {id:'title'},
+//   React.createElement('span', null, 'hey')
+// );
+// ReactDOM.render(el, document.getElementById('container'));
+
+const el = <h1>Hello</h1>;
+
 ReactDOM.render(el, document.getElementById('container'));
